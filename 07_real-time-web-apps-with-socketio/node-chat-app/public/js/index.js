cffa9ff4fcdd8263ef20d814b46b5397e462e17c
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2018-10-27 23:50:33 +0300

    scroll to bottom if user is close to bottom

--- 07_real-time-web-apps-with-socketio/node-chat-app/public/js/index.js
+++ 07_real-time-web-apps-with-socketio/node-chat-app/public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io();
 
+function scrollToBottom() {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    messages.scrollTop(scrollHeight);
+
+    // if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    //     messages.scrollTop(scrollHeight);
+    // }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -19,6 +34,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // console.log('newMessage', message);
 
@@ -47,6 +63,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My Current Location</a>')
--- 07_real-time-web-apps-with-socketio/node-chat-app/server/server.js
+++ 07_real-time-web-apps-with-socketio/node-chat-app/server/server.js
@@ -34,6 +34,7 @@ io.on('connection', (socket) => {
 
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
+    
     });
 
     socket.on('createLocationMessage', (coords) => {

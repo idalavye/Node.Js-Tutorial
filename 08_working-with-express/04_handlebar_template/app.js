54d063f9a68edf29954dc76995634a50a39858af
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-01-26 21:52:58 +0300

    Handlebars: handelbars set default template engine

--- 08_working-with-express/04_handlebar_template/app.js
+++ 08_working-with-express/04_handlebar_template/app.js
@@ -6,15 +6,8 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-/**
- * app.set(); //express projemizde global bir değişken oluşturur. Bunu kullanarak 
- * özel tanımlamalarda yapabiliriz.
- */
-
-/**
- * Projemize bir template engine atadık. Ve template'leri nerede bulabileceğini gösterdik
- */
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs())
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -27,8 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageNotFound: 'Page Not Found' });
 });
 
 app.listen(3000);
---
+++ 08_working-with-express/04_handlebar_template/routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  //render => bu expressjs tarafından sağlanır ve varsayılan şablon motorunu kullanır.
+  res.render('shop', { prods: products, docTitle: 'Shop' , path:'/shop'});
+});
+
+module.exports = router;

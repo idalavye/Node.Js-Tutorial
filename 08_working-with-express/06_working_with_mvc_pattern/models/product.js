e8750b08cf71e2af905e2067464453c4efd17f8f
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-01-29 01:15:24 +0300

    Enhancing the App

--- 08_working-with-express/06_working_with_mvc_pattern/models/product.js
+++ 08_working-with-express/06_working_with_mvc_pattern/models/product.js
@@ -15,9 +15,11 @@ const getProductsFromFile = (callback) => {
 }
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, description, price) {
         this.title = title;
-        this.deneme = 'deneme';
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {

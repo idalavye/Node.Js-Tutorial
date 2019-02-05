ccfc393c7e1b70b6db34483b482a6ba3996929b7
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-05 19:55:39 +0300

    SQL: retrieving data

--- 08_working-with-express/08_working_with_sql_database/util/database.js
+++ 08_working-with-express/08_working_with_sql_database/util/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'locoalhost',
+    host: 'localhost',
     user: 'root',
     database: 'node-shop-db',
     password: 'w4tchDC!iDa'

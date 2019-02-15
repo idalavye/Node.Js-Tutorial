6f265e67eb0cf3a5ad15cf0c23dfefa38921710a
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-15 10:57:53 +0300

    Mongoose: one important thing about fetching relations

--- 08_working-with-express/10_working-with-mongoose/controllers/admin.js
+++ 08_working-with-express/10_working-with-mongoose/controllers/admin.js
@@ -87,6 +87,8 @@ exports.postDeleteProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price -_id') //-_id ile id'nin getirilmemesini sağlayabiliriz.
+    // .populate('userId', 'name -_id') //Sadece ObjectId yi değil user ı tamamen getirmek istiyorsak bu şekilde kullanırız.
     .then(products => {
       res.render('admin/products', {
         prods: products,

bd8cb54754ccf769df80445deb7fbc448b5bf7a7
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-15 10:41:28 +0300

    Mongoose: adding and using user model

--- 08_working-with-express/10_working-with-mongoose/app.js
+++ 08_working-with-express/10_working-with-mongoose/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -18,12 +18,12 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('5c61dfa23f6dd21e444fe6f9').then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     }).catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById('5c666bfe31a0e329d827fc89').then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,6 +33,18 @@ app.use(errorController.get404);
 mongoose
     .connect('mongodb://localhost:27017/shop')
     .then((result) => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'İbrahim',
+                    email: 'idalavye@gmail.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
         app.listen(3000);
     }).catch((err) => {
         console.log(err);

d53234eb5edb20fe1af6d3ecc0ba4ed67130af05
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-16 22:45:28 +0300

    Mongoose: setting a cookie

--- 08_working-with-express/11_sessions-and-cookies/controllers/auth.js
+++ 08_working-with-express/11_sessions-and-cookies/controllers/auth.js
@@ -1,12 +1,28 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = req
+        .get('Cookie')
+        .split(';')[0]
+        .trim()
+        .split('=')[1];
+
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login'
     });
 };
 
-
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true');
     res.redirect('/')
 }
+
+// <!-- <% if(isAuthenticated) {%>
+//     <li class="main-header__item">
+//         <a class="<%= path === '/admin/add-product' ? 'active' : '' %>" href="/admin/add-product">Add Product
+//         </a>
+//     </li>
+//     <li class="main-header__item">
+//         <a class="<%= path === '/admin/products' ? 'active' : '' %>" href="/admin/products">Admin Products
+//         </a>
+//     </li>
+//     <%}%> -->

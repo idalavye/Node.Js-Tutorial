89a5546575a1c559508a3c770d0d31d2fdf727c8
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-19 14:43:46 +0300

    Sessions: deleting a cookie

--- 08_working-with-express/11_sessions-and-cookies/controllers/auth.js
+++ 08_working-with-express/11_sessions-and-cookies/controllers/auth.js
@@ -1,12 +1,6 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    // const isLoggedIn = req
-    //     .get('Cookie')
-    //     .split(';')[0]
-    //     .trim()
-    //     .split('=')[1];
-
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
@@ -20,6 +14,13 @@ exports.postLogin = (req, res, next) => {
         req.session.isLoggedIn = true;
         res.redirect('/')
     }).catch(err => console.log(err))
+}
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    })
 }
 
 // <!-- <% if(isAuthenticated) {%>

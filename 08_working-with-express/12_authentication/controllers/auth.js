b07f75cdd00555b5091f1344c2656c72770b6afc
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-19 15:56:02 +0300

    Auth: encrypting password

--- 08_working-with-express/12_authentication/controllers/auth.js
+++ 08_working-with-express/12_authentication/controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -35,14 +36,16 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
-        return res.redirect('/redirect');
+        return res.redirect('/signup');
       }
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: { items: [] }
       });
-
       return user.save();
     })
     .then(result => {

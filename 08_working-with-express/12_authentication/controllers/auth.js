85ae6335fe630444505b4e0181492f23e6ed4d9f
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-19 15:44:45 +0300

    Auth: implementing an autentication flow

--- 08_working-with-express/12_authentication/controllers/auth.js
+++ 08_working-with-express/12_authentication/controllers/auth.js
@@ -18,16 +18,40 @@ exports.getSignup = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   User.findById('5c68538130205e61c843b6ab').then(user => {
-      req.session.user = user;
-      req.session.isLoggedIn = true;
-      req.session.save().then(err => {
-          console.log(err);
-          res.redirect('/');
-      })
+    req.session.user = user;
+    req.session.isLoggedIn = true;
+    req.session.save().then(err => {
+      console.log(err);
+      res.redirect('/');
+    })
   }).catch(err => console.log(err))
 }
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/redirect');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] }
+      });
+
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      console.log(err);
+    })
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {

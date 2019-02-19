fc8495cd9cd793d0b9b1498bebc8bca8fb24226b
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-19 15:05:32 +0300

    Sessions: making add-to-cart work again

--- 08_working-with-express/11_sessions-and-cookies/app.js
+++ 08_working-with-express/11_sessions-and-cookies/app.js
@@ -31,6 +31,17 @@ app.use(express.static(path.join(__dirname, "public")));
  * saveunitialized: false => kaydedilmesi gerekmeyen bir istek için hiçbir oturumun kaydedilmediğinden emin olun.
  */
 app.use(session({ secret: "secret", resave: false, saveUninitialized: false, store: store }));
+
+app.use((req, res, next) => {
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id).then(user => {
+    req.user = user;
+    next();
+  }).catch(err => console.log(err));
+})
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);

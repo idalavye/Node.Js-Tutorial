4d67da9b72e0418952d5b89a08a76c218ea37867
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-02-23 16:51:43 +0300

    Validation: adding validation to login

--- 08_working-with-express/12_authentication/controllers/auth.js
+++ 08_working-with-express/12_authentication/controllers/auth.js
@@ -22,7 +22,12 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: ''
+    },
+    validationErrors: []
   });
 };
 
@@ -61,14 +66,27 @@ exports.postLogin = (req, res, next) => {
     return res.render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
     });
   }
 
   User.findOne({ email: email }).then(user => {
     if (!user) {
-      req.flash('error', 'Invalid Email or Password');
-      return res.redirect('/login');
+      return res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: 'Invalid Email or Password',
+        oldInput: {
+          email: email,
+          password: password
+        },
+        validationErrors: []
+      });
     }
     bcrypt.compare(password, user.password).then(doMatch => {
       if (doMatch) {
@@ -80,8 +98,16 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/');
         })
       }
-      req.flash('error', 'Invalid Email or Password');
-      res.redirect('/login');
+      return res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        errorMessage: 'Invalid Email or Password',
+        oldInput: {
+          email: email,
+          password: password
+        },
+        validationErrors: []
+      });
     }).catch(err => {
       //password not match
       console.log(err);

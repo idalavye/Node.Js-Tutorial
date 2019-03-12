036b2f4538396c4ee142c37614b91aab433b0a02
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-03-12 17:20:17 +0300

    Upload: handling file uploads with multer

--- 08_working-with-express/12_authentication/app.js
+++ 08_working-with-express/12_authentication/app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -27,7 +28,9 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+//urlencoded sadece text türünde veriler alır. Resim ise binary türünde bir dosyadır. Bu yüzden bunun için ayrı bir kütüphane kullanırız. (multer)
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: 'images' }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
@@ -41,26 +44,41 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => {
-      throw new Error(err);
-    });
-});
-app.use((req, res, next) => {
   /**
    * Aşarıdakiler artık tüm responlarımıza eklenecek. Herbir render metoduna tekrar tekrar eklemiycez
    */
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
-})
+});
+
+app.use((req, res, next) => {
+  /**
+   * Burada error fırlatırsak get500 bunu yakalıyacak ve /500'e redirect yapmaya çalışacak ve tekrak buraya gelecek, bu 
+   * şekilde sonsuz bir döngü içerisinde kalıcak. Bunun için get500 metodunda redirect yerine direk 500.ejs sayfasına 
+   * response yapabiliriz.
+   */
+
+  /**
+   * Express.js error handler syncron hataları yakalayabilirken asyncron hatalar yakalayamaz. Bu hataları yakalaması için 
+   * next(err) şeklinde hata fırlatmalıyız.
+   */
+  // throw new Error('Dummy Error');
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      // throw new Error('asdf');
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      //Bu şekilde errorlar express tarafından yakalanılmıyor.
+      // throw new Error(err);
+      next(new Error(err));
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -68,6 +86,20 @@ app.use(authRoutes);
 
 app.get('/500', errorController.get500);
 app.use(errorController.get404);
+
+/**
+ * next(err) metoduna bir error yerleştirdğimiz zaman express.js bunu anlayacak ve error parametresi bulunmayan tüm 
+ * middleware'leri atlayacaktır. En son aşağıdaki metotda error parametresi olduğu için burada yakalanılacak.
+ */
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(....);
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
   .connect(MONGODB_URI)
--- 08_working-with-express/12_authentication/controllers/admin.js
+++ 08_working-with-express/12_authentication/controllers/admin.js
@@ -15,9 +15,10 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const imageUrl = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  console.log(imageUrl);
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -67,7 +68,11 @@ exports.postAddProduct = (req, res, next) => {
       //   errorMessage: 'Database operation failed, please try again',
       //   validationErrors: []
       // });
-      res.redirect('/500');
+
+      // res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 

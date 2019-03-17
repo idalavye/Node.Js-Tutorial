3a74f6fa08fc3ae162cac128f801a4d13edf624c
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-03-17 19:08:22 +0300

    async: manipulating the dom

--- 08_working-with-express/12_authentication/public/js/admin.js
+++ 08_working-with-express/12_authentication/public/js/admin.js
@@ -2,6 +2,8 @@ const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+    const productElement = btn.closest('article');
+
     /**
      * Şu anda body ile json veri göndermeye çalışırsam backend de bunu encode etmem gerekecekti.
      * Çünlü bodyparser metodunda urlencode kullandık. Bu gelen verileri sadece text olarak alır.
@@ -13,6 +15,9 @@ const deleteProduct = (btn) => {
             'csrf-token': csrf
         }
     }).then(result => {
-        console.log(result);
+        return result.json();
+    }).then(data => {
+        console.log(data);
+        productElement.parentNode.removeChild(productElement);
     }).catch(err => console.log(err));
 };

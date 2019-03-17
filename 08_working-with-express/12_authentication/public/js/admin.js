edf38aef53040cc5b59ed9d54d27f3373da98ea9
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-03-17 19:02:37 +0300

    async: sending and handling background requests

--- 08_working-with-express/12_authentication/public/js/admin.js
+++ 08_working-with-express/12_authentication/public/js/admin.js
@@ -1,4 +1,18 @@
 const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+    /**
+     * Şu anda body ile json veri göndermeye çalışırsam backend de bunu encode etmem gerekecekti.
+     * Çünlü bodyparser metodunda urlencode kullandık. Bu gelen verileri sadece text olarak alır.
+     */
+    //fetch browser tarafından otomatik olarak sunulur.
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    }).then(result => {
+        console.log(result);
+    }).catch(err => console.log(err));
 };

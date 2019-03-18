5cb56260911ffe6e82a5dc60b4ebc279e7a694db
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-03-18 21:57:59 +0300

    Rest: cors errors

--- 08_working-with-express/13_rest_api_project/app.js
+++ 08_working-with-express/13_rest_api_project/app.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+/**
+ * Formdan gelen verileri yakalamak için urlencoded oldukça faydalıdır.
+ */
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+
+/**
+ * application/json
+ * Gelen veriler artık json formatında dönüştürülecek.
+ * smp: const content = req.body.content; 
+ * Yukarıdaki gibi body tagıyla yakalayabilecez.
+ */
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    /**
+     * (*) ile tüm clientlere erişim izni veriyoruz. Buraya bir tane client'de verebilirdik.
+     * (,) kullanarak ta birden fazla client belirtebilirdik.
+     */
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use('/feed', feedRoutes);
 

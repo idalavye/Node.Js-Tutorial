19cf592d5086d994ee9912c4c50529c383d6fce3
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-03-31 22:09:27 +0300

    Rest: adding authorization checks

--- 08_working-with-express/13_rest_api_project/controllers/feed.js
+++ 08_working-with-express/13_rest_api_project/controllers/feed.js
@@ -148,6 +148,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized");
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -169,6 +174,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error("Could not found post");
         error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error("Not authorized");
+        error.statusCode = 403;
         throw error;
       }
       //Check login user

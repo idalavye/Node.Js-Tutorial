830d297814891e72fa29b794c17f2a3b1cc811a8
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-04-02 01:09:51 +0300

    Test: setup and writing first test

---
+++ 08_working-with-express/15_testing/test/start.js
@@ -0,0 +1,19 @@
+const expect = require("chai").expect;
+
+it("should add numbers correctly", function() {
+  const num1 = 2;
+  const num2 = 3;
+
+  //expect returns an object.
+  /**
+   * to burada chai nin sunduğu bir özellik. Testlerimizin ingilizce bir cümle yazıyormuş gibi yazabilmemizi sağlar
+   */
+  expect(num1 + num2).to.equal(5);
+});
+
+it("should not give a result of 6", function() {
+  const num1 = 2;
+  const num2 = 3;
+
+  expect(num1 + num2).not.to.equal(6);
+});

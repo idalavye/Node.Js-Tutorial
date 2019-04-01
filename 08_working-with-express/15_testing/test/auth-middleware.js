cce605b16e7b62654372b5445674a23c8d308a07
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-04-02 01:45:24 +0300

    Test: organizing multiple tests

--- 08_working-with-express/15_testing/test/auth-middleware.js
+++ 08_working-with-express/15_testing/test/auth-middleware.js
@@ -1,23 +1,38 @@
 const authMiddleware = require("../middleware/is-auth");
 const expect = require("chai").expect;
 
-it("should throw an error if no authorization header is present", function() {
-  const req = {
-    get: function() {
-      return null;
-    }
-  };
-  /**
-   * authMiddleware i kendi kullanımım için çağırmıyorum. Test  amacıyla çağırıyorum. Bu yüzden mocha ve chai hataları ayıklayabilmesi için
-   * benim için kendileri çağırmalı. Aşağıdaki gibi bir kullanımda kendim test etmek zorunda kalacağım ve buda test etmenin tüm amacını bozacaktır.
-   * Yani ara katman yazılımını doğrudan kendimiz buraya çağırmak yerine, bunun yerine sadece buna atıfta bulunmaktayız.
-   */
-  //   expect(authMiddleware(req, {}, () => {})).to.throw("Not authenticated.");
+describe("Auth middleware", function() {
+  it("should throw an error if no authorization header is present", function() {
+    const req = {
+      get: function() {
+        return null;
+      }
+    };
+    /**
+     * authMiddleware i kendi kullanımım için çağırmıyorum. Test  amacıyla çağırıyorum. Bu yüzden mocha ve chai hataları ayıklayabilmesi için
+     * benim için kendileri çağırmalı. Aşağıdaki gibi bir kullanımda kendim test etmek zorunda kalacağım ve buda test etmenin tüm amacını bozacaktır.
+     * Yani ara katman yazılımını doğrudan kendimiz buraya çağırmak yerine, bunun yerine sadece buna atıfta bulunmaktayız.
+     */
+    //   expect(authMiddleware(req, {}, () => {})).to.throw("Not authenticated.");
 
-  /**
-   * Artık burada kendimiz çağırmıyoruz.Bunun yerine, nerede olduğumuza göre hazırlandı diyebileceğiniz fonksiyonumuza hazırlıklı bir referans veriyoruz.
-   */
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    "Not authenticated."
-  );
+    /**
+     * Artık burada kendimiz çağırmıyoruz.Bunun yerine, nerede olduğumuza göre hazırlandı diyebileceğiniz fonksiyonumuza hazırlıklı bir referans veriyoruz.
+     */
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if the authorization header is only one string", function() {
+    const req = {
+      get: function() {
+        return "token";
+      }
+    };
+
+    /**
+     * İkinci parametre ve üçüncü parametre üzerinde bir test yapmadığımız için boş bir obje ve boş bir fonksiyon veriyoruz.
+     */
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });

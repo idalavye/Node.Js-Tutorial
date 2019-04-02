c24339b0a5cb677e0194985c9dea9237032c39c4
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-04-02 14:24:46 +0300

    Test: hooks

--- 08_working-with-express/15_testing/test/auth-controller.js
+++ 08_working-with-express/15_testing/test/auth-controller.js
@@ -5,7 +5,32 @@ const mongoose = require("mongoose");
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
-describe("Auth Controller - Login", function() {
+describe("Auth Controller", function() {
+
+/**
+ * before ve after tüm testlerden önce ve sonra sadece bir kez çalışır.
+ * Her testden önce çalışmasını istiyorsak beforeEach, afterEach hooklarını kullanabiliriz.
+ */
+
+  before(function(done) {
+    mongoose
+      .connect("mongodb://localhost:27017/testapp-test")
+      .then(result => {
+        const user = new User({
+          email: "test@test.com",
+          password: "testet",
+          name: "Test",
+          posts: [],
+          _id: "5c0f66b979af55031b34728a"
+        });
+
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
   it("should throw an error with code 500 if accessing the database fails", function(done) {
     sinon.stub(User, "findOne");
     User.findOne.throws();
@@ -35,52 +60,39 @@ describe("Auth Controller - Login", function() {
   });
 
   it("should send a response with a valid user status for an existing users", function(done) {
-    mongoose
-      .connect("mongodb://localhost:27017/testapp-test")
-      .then(result => {
-        const user = new User({
-          email: "test@test.com",
-          password: "testet",
-          name: "Test",
-          posts: [],
-          _id: "5c0f66b979af55031b34728a"
-        });
+    const req = { userId: "5c0f66b979af55031b34728a" };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function(data) {
+        this.userStatus = data.status;
+      }
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("I am new!");
+      /**
+       * Eğer done bu şekilde kullanırsak test hiçbir zaman bitmez. Çünkü mongodb bağlantısının bitmesini bekler.
+       */
+      //   done();
 
-        return user.save();
+      /**
+       * Dummy data ekledikten sonra sonraki testlerde sorun çıkmaması için eklediğimiz şeyleri silmeliyiz.
+       */
+    });
+  });
+
+  after(function(done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
       })
       .then(() => {
-        const req = { userId: "5c0f66b979af55031b34728a" };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function(code) {
-            this.statusCode = code;
-            return this;
-          },
-          json: function(data) {
-            this.userStatus = data.status;
-          }
-        };
-        AuthController.getUserStatus(req, res, () => {}).then(() => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.userStatus).to.be.equal("I am new!");
-          /**
-           * Eğer done bu şekilde kullanırsak test hiçbir zaman bitmez. Çünkü mongodb bağlantısının bitmesini bekler.
-           */
-          //   done();
-
-          /**
-           * Dummy data ekledikten sonra sonraki testlerde sorun çıkmaması için eklediğimiz şeyleri silmeliyiz.
-           */
-          User.deleteMany({})
-            .then(() => {
-              return mongoose.disconnect();
-            })
-            .then(() => {
-              done();
-            });
-        });
-      })
-      .catch(err => console.log(err));
+        done();
+      });
   });
 });

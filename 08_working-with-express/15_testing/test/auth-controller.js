e6e0698bd49552354953339ab88afb29933932cf
İbrahim Dağdelen <ibrahimdagdelentr@gmail.com>
2019-04-02 13:27:05 +0300

    Test: testing asynchronous code

--- 08_working-with-express/15_testing/test/auth-controller.js
+++ 08_working-with-express/15_testing/test/auth-controller.js
@@ -5,11 +5,30 @@ const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", function() {
-  it("should throw an error with code 500 if accessing the database fails", function() {
+  it("should throw an error with code 500 if accessing the database fails", function(done) {
     sinon.stub(User, "findOne");
-    User.findOne.throw();
+    User.findOne.throws();
 
-    expect(AuthController.login)
+    const req = {
+      body: {
+        email: "test@test.com",
+        password: "test"
+      }
+    };
+
+    /**
+     * Eğer burada done kullanmazsak mocha bu test senaryosunun bitmesini beklemez çünkü bu async bir kod. 
+     */
+
+    AuthController.login(req, {}, () => {}).then(result => {
+      expect(result).to.be.an("error");
+      expect(result).to.have.property("statusCode", 500);
+      /**
+       * Done buradaki tüm async işlevlerinin bitmesini bekler ve sonrasında test işleminin geçip geçmediğini söyler.
+       * done aslında mocha tarafında birinci parametrede sağlanan bir fonksiyondur.
+       */
+      done();
+    });
 
     User.findOne.restore();
   });
